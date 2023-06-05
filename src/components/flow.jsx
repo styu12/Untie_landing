ba6c387a93824be5dd93fb666fb139bd4e8b7bb8
styu12 <styu12@naver.com>
2023-06-05 15:48:53 +0900

    Fix: userflow 섹션 수정

--- src/components/flow.jsx
+++ src/components/flow.jsx
@@ -11,29 +11,14 @@ export const Flow = (props) => {
         {props.data ? 
             props.data.map((d, i) => (
                 <div className="flow-container">
-                {/* {i % 2 === 0 ? (
-                    <> */}
-                        <div className="col-md-5 col-xs-12 flow-text-box" key={`${d.title}-${i}`} >
+                        <div className="flow-text-box" key={`${d.title}-${i}`} >
                             <h3 className="flow-title">{d.title}</h3>
                             <p>{d.text}</p>
                         </div>
 
-                        <div className="col-md-7 col-xs-12 flow-image-box" style={{
+                        <div className="flow-image-box" style={{
                             background: `url(${d.image})`,
                         }} />
-                    {/* </>
-                ) : (
-                    <>
-                        <div className="col-md-7 col-xs-12 flow-image-box" style={{
-                            background: `url(${d.image})`,
-                        }}/>
-
-                        <div className="col-md-5 col-xs-12 flow-text-box" key={`${d.title}-${i}`}>
-                            <h3 className="flow-title">{d.title}</h3>
-                            <p>{d.text}</p>
-                        </div>
-                    </>
-                )} */}
                 </div>
         )) : "Loading..."}
 
